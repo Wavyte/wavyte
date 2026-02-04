01e6a84cfb6cb178f4ee48c77a90b7cff899064f
Sanyam Singhal <96378744+sanyam-singhal@users.noreply.github.com>
2026-02-04 03:21:49 +0000

    v0.1.0 - implement easing enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 #![forbid(unsafe_code)]
 
+pub mod anim_ease;
 pub mod core;
 pub mod error;
 
+pub use anim_ease::Ease;
 pub use core::{Canvas, Fps, FrameIndex, FrameRange, Rgba8Premul, Transform2D, Vec2};
 pub use error::{WavyteError, WavyteResult};
